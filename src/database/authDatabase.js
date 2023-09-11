2508e64ed678bd93bf09558ac837368cbd87c39c
gstfrenkel <gst.frenkel@gmail.com>
2023-09-11 17:37:14 -0300

    Restriction fixes for DB

--- src/database/authDatabase.js
+++ src/database/authDatabase.js
@@ -4,6 +4,7 @@ const Exception = require('../services/exception');
 
 async function createUser(username, email, password){
     const prisma = new PrismaClient();
+
     try{
         await prisma.user.create({
             data: {
@@ -14,10 +15,8 @@ async function createUser(username, email, password){
         });
     } catch(err){
         switch (err.code) {
-            case 'P2025': // Unique constraint violation
-                throw new Exception('Username already taken.', 403);
-            case 'P2025': // Primary key violation
-                throw new Exception('Email already used.', 403);
+            case '23505': // Unique constraint violation
+                throw new Exception('Username or email already taken.', 403);
             default:
                 throw new Exception('An unexpected error has occurred. Please try again later.', 500);
           }
@@ -28,6 +27,7 @@ async function createUser(username, email, password){
 
 async function verifyUser(userIdentifier, password){
     const prisma = new PrismaClient();
+
 	try {
         var user = await prisma.user.findFirst({
             where: {
@@ -37,12 +37,8 @@ async function verifyUser(userIdentifier, password){
                 ],
             },
         });
-        const match = bcrypt.compareSync(password, user.password);
-        if(match) {
-            return true;
-        }else{
-            return false;
-        }
+
+        return user && bcrypt.compareSync(password, user.password);
     } catch(err){
         throw new Exception('An unexpected error has occurred. Please try again later.', 500);
     } finally{
