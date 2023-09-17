74a02233e17723983793a27357ba9a6b47b6f1b0
gstfrenkel <gst.frenkel@gmail.com>
2023-09-17 16:59:04 -0300

    Removed unused token exp field

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 function sessionToken(user){
 	return jwt.sign({
         user: user,
-        exp: 3 * Math.floor(Date.now() / 1000) + (60 * 60),
     }, process.env.TOKEN_SECRET_KEY);
 }
 
