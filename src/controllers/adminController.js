eb7b5359bf49e61c150a8386981592c3463bb56e
julianquino <julian.quino2@gmail.com>
2023-12-05 20:09:11 -0300

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -25,6 +25,7 @@ const signIn = async (req, res) => {
         
         res.status(200).json({token: sessionToken(user.username)});
 	} catch(err){
+	    console.log(err);
         res.status(err.statusCode ?? 500).json({ message: err.message ?? 'An unexpected error has occurred. Please try again later.'});
     }
 }
